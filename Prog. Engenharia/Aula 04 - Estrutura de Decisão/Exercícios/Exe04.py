429c5aff3b2bae8b25089461f59b3533c6eba701
Gabriel Antunes Rabello <142711403+Rabellog@users.noreply.github.com>
2024-03-24 23:31:18 -0300

    Add files via upload

---
+++ Prog. Engenharia/Aula 04 - Estrutura de Decisão/Exercícios/Exe04.py
@@ -0,0 +1,8 @@
+n1=float(input("Digite o número 01: "))
+n2=float(input("Digite o número 02: "))
+if (n1>n2):
+    print(f"O número 1 ({n1}) é o maior")
+if (n2>n1):
+    print(f"O número 2 ({n2}) é o maior")
+if (n1==n2):
+    print("Os números são iguais")
